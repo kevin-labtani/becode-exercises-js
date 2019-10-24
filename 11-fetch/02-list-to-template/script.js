a1cf597820a0fa43bd414b839691df3b1b8b89fe
Kevin Labtani <kevinlabtani@gmail.com>
2019-10-24 14:31:08 +0200

    fetch exercice 2

--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -10,5 +10,38 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  const target = document.querySelector("#target");
+
+  document.querySelector("button").addEventListener("click", async function(e) {
+    const response = await fetch(`http://localhost:3000/heroes`);
+    if (response) {
+      const data = await response.json();
+      data.forEach(element => {
+        const list = document.createElement("li");
+        list.setAttribute("class", "hero");
+        target.appendChild(list);
+
+        const heading = document.createElement("h4");
+        heading.setAttribute("class", "title");
+        list.appendChild(heading);
+
+        const str = document.createElement("strong");
+        str.setAttribute("class", "name");
+        str.textContent = element.name;
+        heading.appendChild(str);
+
+        const emphasis = document.createElement("em");
+        emphasis.setAttribute("class", "alter-ego");
+        emphasis.textContent = element.alterEgo;
+        heading.appendChild(emphasis);
+
+        const parag = document.createElement("p");
+        parag.setAttribute("class", "powers");
+        parag.textContent = element.abilities.join(", ");
+        list.appendChild(parag);
+      });
+    } else {
+      throw new Error("Unable to fetch heroes");
+    }
+  });
 })();
