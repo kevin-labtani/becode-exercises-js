1a2cf4da799efed47e2ce241d53cfb047e22a51f
Kevin Labtani <kevinlabtani@gmail.com>
2019-10-24 15:54:25 +0200

    exericise 3 for fetch

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -10,5 +10,44 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  const target = document.querySelector("#target");
+
+  document.querySelector("#hero-id").addEventListener("change", function(e) {
+    heroId = parseInt(e.target.value);
+  });
+  document.querySelector("button").addEventListener("click", async function(e) {
+    const response = await fetch(`http://localhost:3000/heroes`);
+    if (!response) {
+      throw new Error("Unable to fetch heroes");
+    }
+    const data = await response.json();
+    const chosenOne = data.find(element => element.id === heroId);
+
+    if (!chosenOne) {
+      throw new Error("Unable to fetch the hero for that id");
+    }
+
+    const list = document.createElement("li");
+    list.setAttribute("class", "hero");
+    target.appendChild(list);
+
+    const heading = document.createElement("h4");
+    heading.setAttribute("class", "title");
+    list.appendChild(heading);
+
+    const str = document.createElement("strong");
+    str.setAttribute("class", "name");
+    str.textContent = chosenOne.name;
+    heading.appendChild(str);
+
+    const emphasis = document.createElement("em");
+    emphasis.setAttribute("class", "alter-ego");
+    emphasis.textContent = chosenOne.alterEgo;
+    heading.appendChild(emphasis);
+
+    const parag = document.createElement("p");
+    parag.setAttribute("class", "powers");
+    parag.textContent = chosenOne.abilities.join(", ");
+    list.appendChild(parag);
+  });
 })();
